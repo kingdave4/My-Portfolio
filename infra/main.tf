4d834789c46c61bc096e2e845b589f8d87baa3a7
kingdave4 <davidmboli1@gmail.com>
2025-02-04 21:30:41 -0500

    update

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "hugo_rg" {
 
 # Storage Account
 resource "azurerm_storage_account" "hugo_storage" {
-  name                     = "myhugoblogstoragedave"  # Change to a unique name
+  name                     = "myhugoblogstorage2"  # Change to a unique name
   resource_group_name      = azurerm_resource_group.hugo_rg.name
   location                 = azurerm_resource_group.hugo_rg.location
   account_tier             = "Standard"
