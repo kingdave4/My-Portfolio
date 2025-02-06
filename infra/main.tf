d88c56758f361a97e0ec9cd5e30f6e93c6626106
kingdave4 <davidmboli1@gmail.com>
2025-02-05 22:32:21 -0500

    update

--- infra/main.tf
+++ infra/main.tf
@@ -38,16 +38,3 @@ resource "azurerm_cdn_endpoint" "hugo_cdn_endpoint" {
     host_name = azurerm_storage_account.hugo_storage.primary_web_host
   }
 }
-
-# Custom Domain with HTTPS for our website
-resource "azurerm_cdn_endpoint_custom_domain" "hugo_custom_domain" {
-  name            = "hugo-custom-domain"
-  cdn_endpoint_id = azurerm_cdn_endpoint.hugo_cdn_endpoint.id
-  host_name       = "www.davidmboli-idie.com" # Replace with your GoDaddy domain
-
-  cdn_managed_https {
-    certificate_type = "Dedicated"
-    protocol_type    = "ServerNameIndication"
-    tls_version      = "TLS12"
-  }
-}
